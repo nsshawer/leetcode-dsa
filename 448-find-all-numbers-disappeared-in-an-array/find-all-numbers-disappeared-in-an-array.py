dc83610e727233e4c9eec1299a25134615a4fcab
Nada Shawer <nsshawer@gmail.com>
2026-08-31 15:57:39 +0300

    Time: 39 ms (34.92%) | Memory: 31 MB (37.27%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,15 +1,30 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        length = len(nums)
-        hash_table = [0] * (length + 1)
+        # --- O(n) SOLUTION ---
+
+        # length = len(nums)
+        # hash_table = [0] * (length + 1)
+        # result = []
+
+        # for n in nums:
+        #     if n < len(hash_table):
+        #         hash_table[n] += 1
+
+        # for i, h in enumerate(hash_table):
+        #     if h == 0 and i != 0:
+        #         result.append(i)
+        
+        # return result
+
+        # --- O(1) SOLUTION ---
         result = []
 
         for n in nums:
-            if n < len(hash_table):
-                hash_table[n] += 1
-
-        for i, h in enumerate(hash_table):
-            if h == 0 and i != 0:
-                result.append(i)
+            indx = abs(n)-1
+            nums[indx] = -abs(nums[indx])
         
+        for i, n in enumerate(nums):
+            if n > 0:
+                result.append(i+1)
+
         return result
